3cb6ecb25b59da67f28ec9bad29893c8d18971ca
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2021-01-12 18:44:51 +0000

    Update run_release.sh

--- src/ontology/run_release.sh
+++ src/ontology/run_release.sh
@@ -1,5 +1,7 @@
 set -e 
+date
 sh run.sh make prepare_components -B
-sh run.sh make MIR=false all_components -B
+sh run.sh make all_components -B
 sh run.sh make IMP=false prepare_release -B
 sh run.sh make IMP=false all_diffs -B
+date
